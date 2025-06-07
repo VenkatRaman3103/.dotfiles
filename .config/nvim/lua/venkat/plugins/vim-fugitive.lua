ce7e60f8673da0fe17142ab9fdb0a6c3c8bc5bcc
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-06-07 23:37:25 +0530

    update

--- .config/nvim/lua/venkat/plugins/vim-fugitive.lua
+++ .config/nvim/lua/venkat/plugins/vim-fugitive.lua
@@ -1,54 +1,43 @@
 return {
     "tpope/vim-fugitive",
     config = function()
-        -- Configure vim-fugitive to open in tabs instead of splits
         local keymap = vim.keymap.set
 
-        -- Open Git status in new tab (full window)
         keymap('n', '<leader>gs', function()
             vim.cmd('tabnew')
             vim.cmd('Git')
-            vim.cmd('only') -- Close any splits, keep only current window
+            vim.cmd('only')
         end, { desc = 'Git status in new tab' })
 
-        -- Alternative Git status mapping
         keymap('n', '<leader>go', function()
             vim.cmd('tabnew')
             vim.cmd('Git')
             vim.cmd('only')
         end, { desc = 'Git status in new tab' })
 
-        -- Git add all (note: this won't show output, just executes)
         keymap('n', '<leader>ga', ':Git add .<CR>', { desc = 'Git add all files' })
 
-        -- Git commit in new tab
         keymap('n', '<leader>gc', function()
             vim.cmd('tabnew')
             vim.cmd('Git commit')
             vim.cmd('only')
         end, { desc = 'Git commit in new tab' })
 
-        -- Git diff in new tab (use Gvdiffsplit for side-by-side or Git show for single pane)
         keymap('n', '<leader>gd', function()
             vim.cmd('tabnew')
-            vim.cmd('Git show') -- Shows diff in single pane instead of split
+            vim.cmd('Git show')
         end, { desc = 'Git diff in new tab' })
 
-        -- Git log in new tab
         keymap('n', '<leader>gl', function()
             vim.cmd('tabnew')
             vim.cmd('Git log --oneline')
             vim.cmd('only')
         end, { desc = 'Git log in new tab' })
 
-        -- Git blame in new tab
         keymap('n', '<leader>gb', function()
-            vim.cmd('tabnew')
             vim.cmd('Git blame')
-            vim.cmd('only')
         end, { desc = 'Git blame in new tab' })
 
-        -- Additional useful mappings
         keymap('n', '<leader>gp', function()
             vim.cmd('tabnew')
             vim.cmd('Git push')
